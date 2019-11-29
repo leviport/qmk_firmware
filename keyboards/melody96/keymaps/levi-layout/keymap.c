2eddda0d2b277a8f6c6d2dbe36493c231eb3f064
leviport <leviport377@gmail.com>
2019-11-29 12:14:30 -0700

    Fix typos so it builds

--- keyboards/melody96/keymaps/levi-layout/keymap.c
+++ keyboards/melody96/keymaps/levi-layout/keymap.c
@@ -1,5 +1,7 @@
 #include QMK_KEYBOARD_H
 
+#define L0 0
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Layer 0, default layer
@@ -32,7 +34,7 @@ ________________________________________________________________________________
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
     KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, RCTL,    RCTL,    KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT),
+    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RCTL, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT),
 
 
 
@@ -61,8 +63,8 @@ BL_TOGG, BL_DEC, BL_INC changes the in-switch LEDs
 
 
 	LAYOUT(
-    RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, -------, -------, -------, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,
+    RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   RESET,
     _______, _______, KC_HOME, KC_UP,   KC_END,  _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______,
     _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______,
     _______, _______, KC_MPLY, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______,
