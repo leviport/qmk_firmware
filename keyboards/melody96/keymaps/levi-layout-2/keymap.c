317b7b46770e2105c91bcd29f2086412ff60c898
leviport <leviport377@gmail.com>
2020-01-09 07:55:48 -0700

    Some updates to melody96

--- keyboards/melody96/keymaps/levi-layout-2/keymap.c
+++ keyboards/melody96/keymaps/levi-layout-2/keymap.c
@@ -16,25 +16,25 @@ ________________________________________________________________________________
 |    TAB     |   Q    |   W    |   E    |   R    |   T    |   Y    |   U    |   I    |   O    |   P    |  {     |   }    |  |   \      |   7    |   8    |   9    |   +    |
 |____________|________|________|________|________|________|________|________|________|________|________|________|________|_____________|________|________|________|________|
 |              |        |        |        |        |        |        |        |        |        |   ;    |   '    |                    |        |        |        |        |
-|     LCTL     |   A    |   S    |   D    |   F    |   G    |   H    |   J    |   K    |   L    |   :    |   "    |       ENTER        |   4    |   5    |   6    |   +    |
+|     MO_FN    |   A    |   S    |   D    |   F    |   G    |   H    |   J    |   K    |   L    |   :    |   "    |       ENTER        |   4    |   5    |   6    |   +    |
 |______________|________|________|________|________|________|________|________|________|________|________|________|____________________|________|________|________|________|
 |         |        |        |        |        |        |        |        |        |   ,    |    .   |   /    |                |        |        |        |        |        |
 |  SHIFT  |  SHIFT |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |   <    |    >   |   ?    |       SHIFT    |   UP   |   1    |   2    |   3    | ENTER  |
 |_________|________|________|________|________|________|________|________|________|________|________|________|________________|________|________|________|________|________|
 |          |          |          |                                                        |        |        |        |        |        |        |        |        |        |
-|  MO_FN   |   LGUI   | L ALT    |                      SPACE                             | R ALT  |  RCTL  |  RCTL  |  LEFT  |  DOWN  | RIGHT  |   0    |   .    | ENTER  |
+|   LCTL   |   LGUI   | L ALT    |                      SPACE                             | R ALT  |  RCTL  |  RCTL  |  LEFT  |  DOWN  | RIGHT  |   0    |   .    | ENTER  |
 |__________|__________|__________|________________________________________________________|________|________|________|________|________|________|________|________|________|
 */
 
 
 
 	LAYOUT(
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_DEL,  KC_HOME, KC_END,  KC_PGUP, KC_PGDN,
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_DEL,  KC_HOME, KC_END,  KC_PGUP, KC_PGUP,
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO  , KC_BSPC, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+    MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
     KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-    MO(1),   KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RCTL, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT),
+    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RCTL, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT),
 
 
 
