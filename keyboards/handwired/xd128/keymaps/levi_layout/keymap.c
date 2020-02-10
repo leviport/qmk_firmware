706e8c71c40e8fdc37fbac840a1b869bfda8a07b
leviport <leviport377@gmail.com>
2020-02-09 17:57:34 -0700

    update xd128

--- keyboards/handwired/xd128/keymaps/levi_layout/keymap.c
+++ keyboards/handwired/xd128/keymaps/levi_layout/keymap.c
@@ -31,54 +31,112 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* DVORAK
  * .-----------------------------------------------------------------------------------------------------------------------------------------------.
- * |  `~    | 1      | 2      | 3      | 4      | 5      | [      | PSCR   | ]      | 6      | 7      | 8      | 9      | 0      | =      |        |
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |  `~    | 1      | 2      | 3      | 4      | 5      | [      | PSCR   | ]      | 6      | 7      | 8      | 9      | 0      | =      |        |
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |  `~    | 1      | 2      | 3      | 4      | 5      | [      | PSCR   | ]      | 6      | 7      | 8      | 9      | 0      | =      |        |
+ * |        | F1     | F2     | F3     | F4     | F5     | F6     |        |        | F7     | F8     | F9     | F10    | F11    | F12    |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |  `~    | 1      | 2      | 3      | 4      | 5      | [      | PSCR   | ]      | 6      | 7      | 8      | 9      | 0      | =      |        |
+ * |  `~    | 1      | 2      | 3      | 4      | 5      | [      | PSCR   | ]      | ]      | 6      | 7      | 8      | 9      | 0      | =      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | TAB    | '      | ,      | .      | P      | Y      | ESC    | \      | ENT    | F      | G      | C      | R      | L      | /      |        |
+ * | TAB    | '      | ,      | .      | P      | Y      | ESC    | \      | ENT    | ENT    | F      | G      | C      | R      | L      | /      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------+--------|
- * |  LCTL  | A      | O      | E      | U      | I      | HOME   | UNDO   | PG UP  | D      | H      | T      | N      | S      | -      |        |
+ * |  LCTL  | A      | O      | E      | U      | I      | HOME   | UNDO   | PG UP  | PG UP  | D      | H      | T      | N      | S      | -      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------+--------|
- * | LSHIFT | ;      | Q      | J      | K      | X      | END    | UP     | PG DN  | B      | M      | W      | V      | Z      | RSHIFT |        |
+ * | LSHIFT | ;      | Q      | J      | K      | X      | END    | UP     | PG DN  | PG DN  | B      | M      | W      | V      | Z      | RSHIFT |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------+--------|
- * | LCTL   | LGUI   | Z      | LALT   | DEL    | SPACE  | LEFT   | DOWN   | RGHT   |MO(_FN) | BSPC   | DEL    | RALT   | RGUI   | RCTRL  |        |
- * '-----------------------------------------------*------------------------------------*-------------------------------------------------+--------'
+ * | LCTL   | LGUI   | Z      | LALT   | DEL    | SPACE  | LEFT   | DOWN   | RGHT   | RGHT   |MO(_FN) | BSPC   | DEL    | RALT   | RGUI   | RCTRL  |
+ * '-----------------------------------------------*------------------------------------*----------------------------------------------------------'
  */
 
   [_DV] = LAYOUT( /* DVORAK */
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC, KC_PSCR, KC_RBRC, KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC, KC_PSCR, KC_RBRC, KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC, KC_PSCR, KC_RBRC, KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+  KC_NO,   TO(_QW), KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NO,   KC_NO,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC, KC_PSCR, KC_RBRC, KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
   KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_ESC,  KC_BSLS, KC_ENT,  KC_ENT,  KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
   KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_HOME, KC_UNDO, KC_PGUP, KC_PGUP, KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
   KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_END,  KC_UP,   KC_PGDN, KC_PGDN, KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
   KC_LCTL, KC_LGUI, KC_Z,    KC_LALT, KC_DEL,  KC_SPC,  KC_LEFT, KC_DOWN, KC_RGHT, KC_RGHT, MO(_FN), KC_BSPC, KC_DEL,  KC_RALT, KC_RGUI, KC_RCTL
+  ),
+/* QWERTY
+ * .-----------------------------------------------------------------------------------------------------------------------------------------------.
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * |        | F1     | F2     | F3     | F4     | F5     | F6     |        |        | F7     | F8     | F9     | F10    | F11    | F12    |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * |  `~    | 1      | 2      | 3      | 4      | 5      | [      | PSCR   | ]      | ]      | 6      | 7      | 8      | 9      | 0      | =      |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * | TAB    | Q      | W      | E      | R      | T      | ESC    | \      | ENT    | ENT    | Y      | U      | I      | O      | P      | /      |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------+--------|
+ * | LCTL   | A      | S      | D      | F      | G      | HOME   | UNDO   | PG UP  | PG UP  | H      | J      | K      | L      | ;      | -      |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------+--------|
+ * | LSHIFT | Z      | X      | C      | V      | B      | END    | UP     | PG DN  | PG DN  | N      | M      | ,      | .      | '      | RSHIFT |
+ * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------+--------|
+ * | LCTL   | LGUI   | Z      | LALT   | DEL    | SPACE  | LEFT   | DOWN   | RGHT   | RGHT   |MO(_FN) | BSPC   | DEL    | RALT   | RGUI   | RCTRL  |
+ * '-----------------------------------------------*------------------------------------*-------------------------------------------------+--------'
+ */
+  [_QW] = LAYOUT( /* QWERTY  */
+  TO(_DV), KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NO,   KC_NO,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC, KC_PSCR, KC_RBRC, KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_ESC,  KC_BSLS, KC_ENT,  KC_ENT,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_SLSH,
+  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_UNDO, KC_PGUP, KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_MINS,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_UP,   KC_PGDN, KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT, KC_RSFT,
+  KC_LCTL, KC_LGUI, KC_Z,    KC_LALT, KC_DEL,  KC_SPC,  KC_LEFT, KC_DOWN, KC_RGHT, KC_RGHT, MO(_FN), KC_BSPC, KC_DEL,  KC_RALT, KC_RGUI, KC_RCTL
+  ),
+  /* FUNCTION LAYER
+ * .-----------------------------------------------------------------------------------------------------------------------------------------------.
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        | RESET  |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * |        |        |        |        |        |        |        |        |        |        |        | HOME   | UP     | END    |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------+--------|
+ * |        |        |        |        |        |        |        |        |        |        |        | LEFT   | DOWN   | RIGHT  |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------+--------|
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+ * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------+--------|
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+ * '-----------------------------------------------*-----------------------------------------------------------------------------------------------'
+ */
+  [_FN] = LAYOUT( /* FUNCTION LAYER */
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   RESET,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_NO,   KC_NO,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
   )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case QMKBEST:
-      if (record->event.pressed) {
-        // when keycode QMKBEST is pressed
-        SEND_STRING("QMK is the best thing ever!");
-      } else {
-        // when keycode QMKBEST is released
-      }
-      break;
-    case QMKURL:
-      if (record->event.pressed) {
-        // when keycode QMKURL is pressed
-        SEND_STRING("https://qmk.fm/" SS_TAP(X_ENTER));
-      } else {
-        // when keycode QMKURL is released
-      }
-      break;
+    /* case QMKBEST: */
+    /*   if (record->event.pressed) { */
+    /*     // when keycode QMKBEST is pressed */
+    /*     SEND_STRING("QMK is the best thing ever!"); */
+    /*   } else { */
+    /*     // when keycode QMKBEST is released */
+    /*   } */
+    /*   break; */
+    /* case QMKURL: */
+    /*   if (record->event.pressed) { */
+    /*     // when keycode QMKURL is pressed */
+    /*     SEND_STRING("https://qmk.fm/" SS_TAP(X_ENTER)); */
+    /*   } else { */
+    /*     // when keycode QMKURL is released */
+    /*   } */
+    /*   break; */
   }
   return true;
 }
