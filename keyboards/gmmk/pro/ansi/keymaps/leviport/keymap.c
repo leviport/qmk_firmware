8ecce1d7325e54795a489462909ba9d90b89ba28
leviport <levi@system76.com>
2022-03-31 11:58:12 -0600

    Add print screen to my GMMK Pro layout

--- keyboards/gmmk/pro/ansi/keymaps/leviport/keymap.c
+++ keyboards/gmmk/pro/ansi/keymaps/leviport/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     /* Layer 1, QWERTY
-        ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Prt           Rotary(Mute)
+        ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Del           Rotary(Mute)
         ~        1        2        3        4        5        6        7        8        9        0         -       (=)	     BackSpc           Del
         Tab        Q        W        E        R        T        Y        U        I        O        P        [        ]        \               PgUp
         LCTL         A        S        D        F        G        H        J        K        L        ;        "              Enter            PgDn
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        Ct_L     Win_L    Alt_L                               SPACE                               Alt_R    FN       Ct_R     Left     Down     Right
     */
     [2] = LAYOUT(
-        _______, TO(0),   TO(1),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_MPLY,
+        _______, TO(0),   TO(1),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR,          KC_MPLY,
         _______, RGB_TOG, RGB_VAI, RGB_VAD, RGB_HUI, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_MNXT,
         _______,   KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, _______, _______, _______, _______, _______, RESET,          KC_MPRV,
         _______,     KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______, _______, _______, _______, _______,          _______,      _______,
